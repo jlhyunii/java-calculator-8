97285f633a359fee4c83a422fed037dd1a88779e
jlhyunii <jhpark4692@naver.com>
2025-10-19 20:41:13 +0900

    feat(calculator): 연속된 구분자 또는 빈 토큰이 존재할 경우 예외 처리

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -21,13 +21,18 @@ public class Calculator {
         }
 
         else {
-            if (!numbers.matches("^[0-9,:]+$")) {
-                throw new IllegalArgumentException("잘못된 커스텀 구분자 형식입니다. 기본 구분자(, :) 외의 문자가 포함되어 있습니다.");
+            if (numbers.contains(",,") || numbers.contains("::") ||
+                    numbers.contains(",:") || numbers.contains(":,")) {
+                throw new IllegalArgumentException("잘못된 구분자 형식입니다. 연속된 구분자가 존재합니다.");
             }
 
             if (numbers.startsWith(",") || numbers.startsWith(":") ||
                     numbers.endsWith(",") || numbers.endsWith(":")) {
                 throw new IllegalArgumentException("잘못된 구분자 형식입니다. 입력이 구분자로 시작하거나 끝날 수 없습니다.");
+            }
+
+            if (!numbers.matches("^[-0-9,:a-zA-Z\\s]+$")) {
+                throw new IllegalArgumentException("잘못된 커스텀 구분자 형식입니다. 기본 구분자(, :) 외의 문자가 포함되어 있습니다.");
             }
         }
 
@@ -35,14 +40,18 @@ public class Calculator {
         int sum = 0;
 
         for (String token : tokens) {
+            if (token.isBlank()) {
+                throw new IllegalArgumentException("빈 토큰이 존재합니다.");
+            }
+
             if (!token.matches("-?\\d+")) {
-                throw new IllegalArgumentException("숫자가 아닌 값이 포함되어 있습니다 : " + token);
+                throw new IllegalArgumentException("숫자가 아닌 값이 포함되어 있습니다.");
             }
 
             int number = Integer.parseInt(token);
 
             if (number < 0) {
-                throw new IllegalArgumentException("음수는 허용되지 않습니다 : " + number);
+                throw new IllegalArgumentException("음수는 허용되지 않습니다.");
             }
 
             sum += number;
