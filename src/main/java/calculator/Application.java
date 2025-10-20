64855be7ce13bdfbf18c7ab5f2dca52da38a6693
jlhyunii <jhpark4692@naver.com>
2025-10-20 23:32:55 +0900

    fix(main): ApplicationTest 실패 문제 해결

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,19 +1,22 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.NoSuchElementException;
 
 public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
-        String input = Console.readLine();
 
+        String input;
         try {
-            Calculator calculator = new Calculator();
-            int result = calculator.add(input);
-
-            System.out.println("결과 : " + result);
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
+            input = Console.readLine();
+        } catch (NoSuchElementException e) {
+            input = "";
         }
+
+        Calculator calculator = new Calculator();
+        int result = calculator.add(input);
+
+        System.out.println("결과 : " + result);
     }
 }
